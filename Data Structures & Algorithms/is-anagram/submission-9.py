c7c2d518c0576d4ae7d0961768aa010eb115d125
Danica Navarro <daninavarrodden@gmail.com>
2026-07-27 21:07:56 -0700

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hashmap_s = {}
+        hashmap_t = {}
+
+        for i in s:
+            if i not in hashmap_s:
+                hashmap_s[i] = 1
+            else:
+                hashmap_s[i] += 1
+
+        for j in t:
+            if j not in hashmap_t:
+                hashmap_t[j] = 1
+            else:
+                hashmap_t[j] += 1
+        
+        if hashmap_s != hashmap_t:
+            return False
+        return True
